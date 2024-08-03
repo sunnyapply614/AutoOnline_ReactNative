49e37818bf51b79114aaebd97662d8b0681f37af
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-03 14:54:07 +0300

    Update route.tsx

--- app/api/send-contact/route.tsx
+++ app/api/send-contact/route.tsx
@@ -1,9 +1,11 @@
+
+//app/api/send-contact/route.tsx
 import { NextResponse } from 'next/server'
 import { sendContact } from '../../sendEmail'
 
 export async function POST(request: Request) {
 
-    
+    const data = await request.json();
 
     try {
         await sendContact(data.name, data.email,
