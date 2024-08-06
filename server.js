7c23530a358e114540e436d0d90cd1457432af1e
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-06 04:29:24 +0300

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,10 @@ const { createServer } = require('http')
 const { parse } = require('url')
 const next = require('next')
 
-
+const dev = process.env.NODE_ENV !== 'production'
+const port = process.env.PORT || 3000;
+const app = next({ dev })
+const handle = app.getRequestHandler()
 
 app.prepare().then(() => {
   createServer((req, res) => {
