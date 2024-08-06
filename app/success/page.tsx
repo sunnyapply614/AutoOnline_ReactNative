48801d3c1a7750e382c99c892bb9583eab5d7f6f
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-06 04:32:07 +0300

    Update page.tsx

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -10,10 +10,7 @@ const Success = function () {
                 <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl text-center">Success</h1>
             </section>
 
-            <h4 className="text-center text-[#002E5B] text-[28px] font-sans font-extrabold mt-12">Your message was sent successfully.</h4>
-            <h4 className="text-center text-[#002E5B] text-[28px] font-sans font-bold mt-4">We will contact soon…</h4>
-            <h4 className="text-center text-[#002E5B] text-[28px] font-sans font-bold mt-4">If you need immediate assistance,</h4>
-            <Link href={CALL_TO} className="text-center text-[#545454] text-[28px] font-sans mt-4 font-bold">{PHONE_NUMBER}</Link>
+
 
         </main>
     )
