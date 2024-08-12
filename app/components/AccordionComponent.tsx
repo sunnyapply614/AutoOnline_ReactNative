8a98c8c83ce6f0f86370f6de3187c968fdccc46e
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-12 08:38:26 +0300

    Update AccordionComponent.tsx

--- app/components/AccordionComponent.tsx
+++ app/components/AccordionComponent.tsx
@@ -89,7 +89,11 @@ const AccordionComponent = function () {
                 index == selectedIndex ? <svg data-accordion-icon className="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg> : <svg data-accordion-icon className="w-6 h-6 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
               }
             </button>
-
+            <div id="body" className={selectedIndex == index ? "h-48" : "h-48 hidden"} >
+              <div className="p-5 border border-blue-500">
+                <p className="mb-2 text-gray-500 ">{item.answer}</p>
+              </div>
+            </div>
             
           </li>
         ))
