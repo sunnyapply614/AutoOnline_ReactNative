965f5a90e42e60942bf0159fc82c151673e931d6
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-15 05:42:19 +0300

    Update SendMailForm.tsx

--- app/components/SendMailForm.tsx
+++ app/components/SendMailForm.tsx
@@ -1,7 +1,6 @@
 "use client"
 
-import { useState } from "react"
-import { useRouter } from "next/navigation";
+
 
 export const SendMailForm = function () {
     const [fullnameMsg, setFullNameMsg] = useState("");
