51e457542b78045f1dbdcdaef793682eaf7636d1
Impact Design | Fashionable Website | Smart Code <ozellamaryleone@gmail.com>
2024-08-15 05:44:05 +0300

    Update page.tsx

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -6,7 +6,9 @@ const Success = function () {
     return (
         <main className="flex flex-col bg-white mt-24">
             <PhoneIcon />
-
+            <section className="bg-center bg-no-repeat bg-gray-300 bg-blend-multiply py-24 " style={{ backgroundImage: "url('/about-bg.webp')" }}>
+                <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl text-center">Success</h1>
+            </section>
 
             <h4 className="text-center text-[#002E5B] text-[28px] font-sans font-extrabold mt-12">Your message was sent successfully.</h4>
             <h4 className="text-center text-[#002E5B] text-[28px] font-sans font-bold mt-4">We will contact soon…</h4>
